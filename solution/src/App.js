9e3b19fdaae45d4750a033d61541e59126e6187f
abhishekdev45 <abhishek14kl@gmail.com>
2023-12-28 13:29:48 +0530

    bbb

--- solution/src/App.js
+++ solution/src/App.js
@@ -1,5 +1,5 @@
-import { useState } from "react"
-import mobile from "./images/image-hero-mobile.png"
+import { useState  , useEffect} from "react"
+import mobile from "./images/image-hero-desktop.png"
 import desktop from "./images/image-hero-desktop.png"
 import audiophile from "./images/client-audiophile.svg"
 import databiz from "./images/client-databiz.svg"
@@ -18,6 +18,7 @@ import planning from "./images/icon-planning.svg"
 function App() {
   const [openFeatures, setOpenFeatures] = useState(false)
   const [openCompany, setOpenCompany] = useState(false)
+  const [scrolling, setScrolling] = useState(false);
 
   const handleOpenMenu = () => {
     const navbar = document.querySelector(".navbar")
@@ -32,13 +33,32 @@ function App() {
     }
   }
 
+  
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setScrolling(true);
+      } else {
+        setScrolling(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
-      <header className="p-5 flex items-center justify-between">
+      <header className={`bg-headerHover lg:fixed lg:top-0 left-0 right-0 p-5 flex items-center justify-between ${
+          scrolling ? "bg-headerHover" : "bg-header"
+        } transition-bg-color`}>
         <div className="lg:flex lg:items-start lg:justify-start">
-          <img src={logo} alt="" className="lg:mr-5" />
-          <nav className="navbar">
-            <div>
+          <img src={logo} alt="" className="lg:mr-12" />
+          <nav className="navbar max-auto">
+            <div className="ml-8">
               <button
                 onClick={() => setOpenFeatures(!openFeatures)}
                 className="flex items-center justify-start"
@@ -68,7 +88,7 @@ function App() {
               )}
             </div>
 
-            <div>
+            <div className="ml-8">
               <button
                 onClick={() => setOpenCompany(!openCompany)}
                 className="flex items-center justify-start"
@@ -94,10 +114,10 @@ function App() {
                 </ul>
               )}
             </div>
-            <div className="mb-2 lg:mb-0">
+            <div className="mb-2 lg:mb-0 ml-8">
               <button>Careers</button>
             </div>
-            <div>
+            <div className="ml-8">
               <button>About</button>
             </div>
           </nav>
@@ -117,54 +137,49 @@ function App() {
         </div>
       </header>
 
-      <section className="flex flex-col-reverse py-20 md:grid md:gap-10 md:grid-cols-2 md:place-items-center max-w-4xl lg:max-w-5xl md:mx-auto">
-        <article className="text-center md:text-left px-5 xl:px-0 mt-10 md:mt-0">
-          <h1 className="font-bold text-4xl lg:text-6xl xl:text-7xl mb-5">
-            Make remote work
-          </h1>
-          <p className="mb-5">
-            Get your team in sync, no matter your location. Streamline
-            processes, create team rituals, and watch productivity soar.
-          </p>
-          <button className="bg-black rounded-lg shadow text-white font-bold hover:opacity-75 transition-all duration-150 pt-3 pb-2 px-6 cursor-pointer">
-            Learn more
-          </button>
+      <section className="bg-section py-20 md:grid md:gap-10 md:grid-cols-2 md:place-items-center max-auto">
+  
 
-          <ul className="grid grid-cols-4 gap-1 mt-10 place-items-center md:place-items-start">
-            <li>
-              <img src={databiz} alt="" className="w-16 md:w-24" />
-            </li>
-            <li>
-              <img src={audiophile} alt="" className="w-16 md:w-24" />
-            </li>
-            <li>
-              <img src={meet} alt="" className="w-16 md:w-24" />
-            </li>
-            <li>
-              <img src={maker} alt="" className="w-16 md:w-24" />
-            </li>
-          </ul>
-        </article>
+  <article className="md:order-2 text-center md:text-left px-5 xl:px-0 md:mt-0">
+    <h1 className="font-bold text-4xl lg:text-6xl xl:text-7xl mb-5">
+      Dummy Header
+    </h1>
+    <p className="mb-5">
+      Get your team in sync, no matter your location. Streamline processes,
+      create team rituals, and watch productivity soar. Get your team in sync, no matter your location. Streamline processes,
+      create team rituals, and watch productivity soar. Get your team in sync, no matter your location. Streamline processes,
+      create team rituals, and watch productivity soar.
+    </p>
+    <button className="bg-black rounded-lg shadow text-white font-bold hover:opacity-75 transition-all duration-150 pt-3 pb-2 px-6 cursor-pointer">
+      Get Started
+    </button>
 
-        <article>
-          <picture>
-            <source media="(min-width: 768px)" srcSet={desktop} />
-            <img src={mobile} alt="" className="w-full" />
-          </picture>
-        </article>
-      </section>
+    <ul className="grid grid-cols-4 gap-1 mt-10 place-items-center md:place-items-start">
+      <li>
+        <img src={databiz} alt="" className="w-16 md:w-24" />
+      </li>
+      <li>
+        <img src={audiophile} alt="" className="w-16 md:w-24" />
+      </li>
+      <li>
+        <img src={meet} alt="" className="w-16 md:w-24" />
+      </li>
+      <li>
+        <img src={maker} alt="" className="w-16 md:w-24" />
+      </li>
+    </ul>
+  </article>
+  <article className="md:order-1 mt-10 md:mt-0 md:mr-5">
+    <picture>
+      <source media="(min-width: 768px)" srcSet={desktop} />
+      <img src={mobile} alt="" className="w-full md:h-auto" />
+    </picture>
+  </article>
+</section>
 
-      <div className="attribution">
-        Challenge by{" "}
-        <a
-          href="https://www.frontendmentor.io?ref=challenge"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Frontend Mentor
-        </a>
-        . Coded by <a href="https://tsbsankara.netlify.app">Thomas Sankara</a>.
-      </div>
+
+
+    
     </>
   )
 }
--- solution/tailwind.config.js
+++ solution/tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        header: "#f6f6f4",
+        headerHover: "white",
+        section: "#f6f6f4",
+        sectionHover: "white",
+      },
+      transitionProperty: {
+        'bg-color': 'background-color',
+      },
+    },
   },
   plugins: [],
 }
